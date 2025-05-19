5d2ec775d6f0a17ba35b79a9944cc1783d90b36c
nickdadiala <nick2771910@outlook.com>
2025-05-19 13:12:55 +1200

    new-app-folder

---
+++ new-app-folder.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "Stop"
+$sourceTemplate = ".\appliaction-template"
+
+if(-not (Test-path $sourceTemplate -PathType Container)) {
+
+}
+
+$applicationName = Read-Host "Enter new application name, no whitespaces"
+
+if([string]::IsNullOrWhiteSpace($applicationName)) {
+    throw "no application name entered"
+}
+
+foreach($charitem in $applicationName.ToCharArray()) {
+    if([string]::IsNullOrWhiteSpace($charitem.ToString())) {
+        throw "whitespace characters not allowed"
+    }
+}
