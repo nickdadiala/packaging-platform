c6a538ce1f8252c85f3e4781023c3581097b932c
nickdadiala <nick2771910@gmail.com>
2025-07-09 16:15:17 +1200

    UserObject

--- UserObjectID.ps1
+++ UserObjectID.ps1
@@ -1,11 +1,11 @@
 # To Export the ObjectID from the AAD for each user run the script"UserObjectID.ps1"
 Connect-Module -Microrgaph
-If (-not(Install-Module -Microrgaph).$check)
+$check= Read-Host "Micrograph Module Installed"
+If (-not(Install-Module -Micrograph).$check) {
+}
   Install-Module -Microrgaph
-Write-Output -check(-Microrgaph) is installed
+Write-Output -check(-Microrgaph) is installed...
 
 Get-Aduser -filter {-Department= 'ObjectID'}, {-Department='IT'}, {-Department='All'} | Format-Table Name, UserID, ObjectID.
 
 Write-Output "ExportObjectID.csv "
-
-
