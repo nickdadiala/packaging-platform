77cadb330eb030636c670f0d49f153ec307f98a2
nickdadiala <nick2771910@gmail.com>
2025-07-09 16:40:09 +1200

    updatescriptUserObject

--- UserObjectID.ps1
+++ UserObjectID.ps1
@@ -1,11 +1,18 @@
 # To Export the ObjectID from the AAD for each user run the script"UserObjectID.ps1"
-Connect-Module -Microrgaph
-$check= Read-Host "Micrograph Module Installed"
-If (-not(Install-Module -Micrograph).$check) {
-}
-  Install-Module -Microrgaph
-Write-Output -check(-Microrgaph) is installed...
+#Connect-Module -Microrgaph
+$prerequisites= @(
+    @{Name= "Micrograph"; check="Get-ItemProperty -Path 'HKLM\Software\Microsoft\Windows\Powershell -Name version -ErrorAction -SilentlyContinue'"; Install="Start-process -Filepath C:\Windows\Powershell"}
+)
+ foreach($prereqs in $prerequisites) {
+    Write-Output "checck$($prereqs.name)"
+    if(-not(Invoke-Expression $prereqs.check)){
+        Write-Output $($prereqs.name) not installed
+    }
+ }
 
-Get-Aduser -filter {-Department= 'ObjectID'}, {-Department='IT'}, {-Department='All'} | Format-Table Name, UserID, ObjectID.
+
+Get-Aduser -filter {-Department= 'All IT'}, {-Department='IT'}, {-Department='All'} | Format-Table Name, UserID, ObjectID.
 
 Write-Output "ExportObjectID.csv "
+
+Get-Aduser -Filter {}
