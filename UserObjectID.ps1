df957ec1fd543b6d4e661a1b36cf6ee03649a4ff
nickdadiala <nick2771910@gmail.com>
2025-07-09 13:54:55 +1200

    UserObjectID

---
+++ UserObjectID.ps1
@@ -0,0 +1,8 @@
+# To Export the ObjectID from the AAD for each user run the script"UserObjectID.ps1"
+Connect-Module -Microrgaph
+If (-not(Install-Module -Microrgaph).$check)
+  Install-Module -Microrgaph
+Write-Output -check(-Microrgaph) is installed
+
+Get-Aduser -filter {-Department= 'ObjectID'}
+
